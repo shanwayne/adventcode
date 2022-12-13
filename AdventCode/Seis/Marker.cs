c0e70bfc268301b514321b107cd7d6a4f96876b0
Almando Mason <amason@highresbio.com>
2022-12-13 12:04:38 -0500

    Day 6

--- AdventCode/Seis/Marker.cs
+++ AdventCode/Seis/Marker.cs
@@ -1,35 +1,33 @@
-﻿namespace AdventCode.Seis
-{
-    public class DaySix
-    {
-        public static void Run()
-        {
-            Console.WriteLine("******");
-            Console.WriteLine("Day 6");
-            var lines = File.ReadAllLines("Seis/Input.txt");
-            Console.WriteLine($"Markers: {string.Join(" ", FindMarkers(lines, 4))}");
-            Console.WriteLine($"Markers: {string.Join(" ", FindMarkers(lines, 14))}");
-        }
+﻿namespace AdventCode.Seis;
 
-        private static IEnumerable<int> FindMarkers(IEnumerable<string> lines, int takeAmount)
-        {
-            var markers = lines.Select(line =>
-            {
-                for (var i = 0; i < line.Length; i++)
-                {
-                    var duplicateFound = line.Skip(i)
-                        .Take(takeAmount)
-                        .GroupBy(letter => letter)
-                        .Any(group => group.Count() > 1);
-                    if (duplicateFound)
-                        continue;
-                    return i + takeAmount;
-                }
-                return 0;
-            })
-                .ToList();
-            return markers;
-        }
+public class DaySix
+{
+    public static void Run()
+    {
+        Console.WriteLine("******");
+        Console.WriteLine("Day 6");
+        var lines = File.ReadAllLines("Seis/Input.txt");
+        Console.WriteLine($"Markers: {string.Join(" ", FindMarkers(lines, 4))}");
+        Console.WriteLine($"Markers: {string.Join(" ", FindMarkers(lines, 14))}");
     }
 
+    private static IEnumerable<int> FindMarkers(IEnumerable<string> lines, int takeAmount)
+    {
+        var markers = lines.Select(line =>
+        {
+            for (var i = 0; i < line.Length; i++)
+            {
+                var duplicateFound = line.Skip(i)
+                    .Take(takeAmount)
+                    .GroupBy(letter => letter)
+                    .Any(group => group.Count() > 1);
+                if (duplicateFound)
+                    continue;
+                return i + takeAmount;
+            }
+            return 0;
+        })
+        .ToList();
+        return markers;
+    }
 }
